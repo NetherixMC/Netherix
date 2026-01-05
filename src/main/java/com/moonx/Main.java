8a8c2ffc9d1db8e17dba11f87806881618a6e574
MoonX388 <moonx388@gmail.com>
2026-01-05 21:20:22 +0700

    update Main.java

--- src/main/java/com/moonx/Main.java
+++ src/main/java/com/moonx/Main.java
@@ -5,7 +5,16 @@ import com.moonx.gui.MainGUI;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize and check for updates
-        RuntimeBootstrap.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Netherix runtime shutting down...");
+            RuntimeBootstrap.shutdown();
+        }));
+    
+        try {
+            RuntimeBootstrap.start();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
     }
 }
